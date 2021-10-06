e31b1f06e8571f37766fd7f6ff17ab688a71a8ff
dglabella <dglabella@gmail.com>
2021-10-06 11:55:30 -0300

    Update App.java

--- evac/src/main/java/ar/edu/unsl/App.java
+++ evac/src/main/java/ar/edu/unsl/App.java
@@ -14,23 +14,6 @@ public final class App {
     }
 
     public static void main(String[] args) {
-        // try {
-
-        // int i, j;
-        // i = 0;
-        // j = 0;
-
-        // List<int[]> coords = Neighborhood.Moore(i, j, 1, 8, 4, false);
-
-        // System.out.println("(" + i + "," + j + ") neighbors ->");
-        // for (int n = 0; n < coords.size(); n++) {
-        // System.out.println("\t(" + coords.get(n)[0] + "," + coords.get(n)[1] + ")");
-        // }
-        // } catch (Exception e) {
-        // // TODO Auto-generated catch block
-        // e.printStackTrace();
-        // }
-
         Engine engine = new Engine(
                 new EnviromentGenerator().generateEnviroment2(App.WIDTH, App.HEIGHT), App.SUBSTEPS);
         engine.execute();
